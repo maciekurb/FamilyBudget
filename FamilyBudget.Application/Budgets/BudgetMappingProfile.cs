3acb3b9c6d2c51a48612b024d402eef7e5159844
maciek.urb <m.z.urbanczyk@gmail.com>
2023-06-06 13:41:56 +0200

    Add AutoMapper to API and create BudgetMappingProfile

---
+++ FamilyBudget.Application/Budgets/BudgetMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FamilyBudget.Application.Budgets.DTOs;
+using FamilyBudget.Domain.Entities;
+
+namespace FamilyBudget.Application.Budgets;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Budget, BudgetDto>();
+        CreateMap<BudgetDto, Budget>();
+    }
+}
