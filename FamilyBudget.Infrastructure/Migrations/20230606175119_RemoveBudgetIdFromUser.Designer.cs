18602adeb5ae9b61260fe80b4cdfaa9605d0f482
maciek.urb <m.z.urbanczyk@gmail.com>
2023-06-06 21:37:26 +0200

    Cleanup migrations

--- FamilyBudget.Infrastructure/Migrations/20230606175119_RemoveBudgetIdFromUser.Designer.cs
+++ FamilyBudget.Infrastructure/Migrations/20230606175119_RemoveBudgetIdFromUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FamilyBudget.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FamilyBudget.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230606175119_RemoveBudgetIdFromUser")]
+    partial class RemoveBudgetIdFromUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
